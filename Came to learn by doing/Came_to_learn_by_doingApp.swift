d322cd41710eff1d7edac714dac1d5ad4d46ff3f
Alan Casas <alancasasarevalo@gmail.com>
2021-06-16 20:21:41 +0200

    Initial Commit

---
+++ Came to learn by doing/Came_to_learn_by_doingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Came_to_learn_by_doingApp.swift
+//  Came to learn by doing
+//
+//  Created by Alan Casas on 16/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Came_to_learn_by_doingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
